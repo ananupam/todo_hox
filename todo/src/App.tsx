b913373669f093871e5cbb680c94593c88065c1a
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-03-08 14:38:58 +0530

    setting up repo

---
+++ todo/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Main from './components/main';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hii</h1>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/components/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main =() => {
+    return(
+        <div className='optionbtns'>
+        <button>New Task</button>
+        <button>Delete Task</button>
+        <button>Update Task</button>
+        <button>List Task</button>
+        <button>Reorder task</button>
+    </div>
+    )
+}
+
+export default Main
